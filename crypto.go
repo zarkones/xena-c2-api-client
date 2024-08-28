88f52a424008b71c46faae844f891eea2e7ce95a
zarkones <zarkones.xena@gmail.com>
2024-08-28 04:00:49 +0200

    better error handling when requesting pub key

--- crypto.go
+++ crypto.go
@@ -1,15 +1,15 @@
 package c2api
 
 import (
-	"errors"
 	"io"
 	"net/http"
 )
 
-var ErrKeyIsNil = errors.New("key is nil")
-var ErrTrustedKeyIsNil = errors.New("trusted public key is not set")
-
 func GetC2PublicKey() (pubKeyPEM []byte, err error) {
+	if len(*AuthToken) == 0 {
+		return nil, ErrNilAuthToken
+	}
+
 	req, err := http.NewRequest(http.MethodGet, *BaseURL+"/v1/public-key", nil)
 	if err != nil {
 		return nil, err
--- errors.go
+++ errors.go
@@ -2,4 +2,9 @@ package c2api
 
 import "errors"
 
-var ErrUnexpectedStatusCode = errors.New("unexpected status code")
+var (
+	ErrUnexpectedStatusCode = errors.New("unexpected status code")
+	ErrKeyIsNil             = errors.New("key is nil")
+	ErrTrustedKeyIsNil      = errors.New("trusted public key is not set")
+	ErrNilAuthToken         = errors.New("api authentication token is nil")
+)
